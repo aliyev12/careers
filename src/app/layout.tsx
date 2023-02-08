725403e63f4724750237c12f7ef9cb49273214a9
Abdul Aliyev <aaa7c4@gmail.com>
2023-02-07 21:16:58 -0500

    Add google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,32 @@
-import './globals.css'
+import { Nunito } from "@next/font/google";
+import "./globals.css";
+import { Navbar } from "./Navbar";
+
+const nunito = Nunito({
+  variable: "--nunito-font",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  // @TODO make lang="en" dynamic
   return (
-    <html lang="en">
+    <html lang="en" className={nunito.variable}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+        <footer>footer</footer>
+      </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: "var(--nunito-font)",
+      },
+    },
   },
   plugins: [],
 };
